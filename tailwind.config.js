d557573cc7ee8336ecc04de1d8525640b49e61b9
Vijayavedhasekarn <vedhatech02@gmail.com>
2023-11-03 22:26:33 +0530

    boilerplate to hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ export default {
       fontFamily: {
         'poppins': "'Poppins', sans-serif",
         'inter': "'Inter', sans-serif",
+      },
+      backgroundImage: {
+        'hero-bg': 'url("/public/images/BACKGROUND.png")'
       }
     },
   },
